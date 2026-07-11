00916497136cbe0591674292ecd7e76f4eeb1ce1
Divya Jyoti Chaudhary <79772150+divya00222@users.noreply.github.com>
2026-07-11 20:05:35 +0545

    Delete orchid-cafe,-lahan (12) directory

--- orchid-cafe,-lahan (12)/includes/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Orchid Cafe - Database Compatibility Wrapper
- * For Hostinger/cPanel Shared Hosting
- */
-
-// Simply include the main configuration file which now contains the DB connection
-require_once __DIR__ . '/config.php';
